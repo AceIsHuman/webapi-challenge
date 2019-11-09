32ae8d59867ff084073b2bb43d971cb471a19665
AceIsHuman <menoman1995@yahoo.com>
2019-11-09 12:05:25 -0600

    create endpoint to get project by id

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -3,6 +3,7 @@ const projectsModel = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
+// GET ALL PROJECTS
 router.get('/', async (req, res) => {
   try {
     const projects = await projectsModel.get();
@@ -10,6 +11,19 @@ router.get('/', async (req, res) => {
     else return res.status(404).json({ message: "There is currently no projects."});
   } catch (err) {
     return res.status(500).json({ errorMessage: "Unable to retrieve projects", error: err });
+  }
+});
+
+// GET PROJECT BY ID
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const project = await projectsModel.get(id);
+    if (project) return res.status(200).json(project);
+    else return res.status(404).json({ message: "A project with that id does not exist." });
+  }
+  catch (err) {
+    return res.status(500).json({ errorMessage: "Unable to retrive project." });
   }
 });
 
